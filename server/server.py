8de1fbfbe74fc0ff1a104032fe67f5e7060ea5fb
Ricardo Pelaez Botero <ricardopelaezbotero@AirdeRicardo2.lan>
2026-04-25 17:23:08 +0200

    fix indentation properly

--- server/server.py
+++ server/server.py
@@ -1,12 +1,12 @@
-    import sys
-    import os.path
+import sys
+import os
+import os.path
 
-    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-    import gaurabda as G
-    import gaurabda.TServer as GS
+import gaurabda as G
+import gaurabda.TServer as GS
 
+GCAL_PORT = int(os.getenv("PORT", os.getenv("GCAL_SERVER_PORT", 8047)))
 
-    GCAL_PORT = int(os.getenv("PORT", os.getenv("GCAL_SERVER_PORT", 8047)))
-
-    GS.run_server(host="0.0.0.0", port=GCAL_PORT)
+GS.run_server(host="0.0.0.0", port=GCAL_PORT)
