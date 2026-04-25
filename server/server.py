635ac3ad5026169ba6ed5c289ef0fede00864825
Ricardo Pelaez Botero <ricardopelaezbotero@AirdeRicardo2.lan>
2026-04-25 17:28:38 +0200

    fix render port only

--- server/server.py
+++ server/server.py
@@ -7,6 +7,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 import gaurabda as G
 import gaurabda.TServer as GS
 
-GCAL_PORT = int(os.getenv("PORT", os.getenv("GCAL_SERVER_PORT", 8047)))
+GCAL_PORT = int(os.environ.get("PORT", 10000))
 
-GS.run_server(host="0.0.0.0", port=GCAL_PORT)
+GS.run_server(port=GCAL_PORT)
